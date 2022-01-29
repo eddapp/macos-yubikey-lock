c86f50bd6c31a6adf0832e37285433164bdf736c
@WuKogan <20248804+eddapp@users.noreply.github.com>
2022-01-29 08:56:30 -0700

    file location change
    the file location for the osx-lock-on-remove-usb.sh file can not go into /opt/ there is not directory for macOS 10.13.6 (17G14033) and homebrew. I am aware of the unsupported outdated etc behavior language etc. This location change for cp the file allows for the program to work properly now. so for older MacOS users :)

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 
-cp osx-lock-on-remove-usb.sh /opt/homebrew/bin
-chmod +x /opt/homebrew/bin/osx-lock-on-remove-usb.sh
+cp osx-lock-on-remove-usb.sh /usr/local/Homebrew/bin
+chmod +x /usr/local/Homebrew/bin/osx-lock-on-remove-usb.sh
 cp com.fluffypony.osx-lock-on-remove-usb.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/com.fluffypony.osx-lock-on-remove-usb.plist
